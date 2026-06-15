a139b90d4925d225e0c48324ed4be3377ad5f31b
athreya <akondiathreya@gmail.com>
2026-06-15 16:46:43 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 5454;
+
+
+app.get("/", (req, res) => {
+    res.send("<h1>Backend Application is running</h1>");
+});
+
+let counter = 0;
+
+app.get("/api/v1", (req, res) => {
+    counter = counter + 1;
+    return res.status(200).json({
+        message: "Api is working",
+        counter: counter
+    })
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at port : ${PORT}`);
+});
